5601f14b96d624dfdc7dd3fac8170d5c15d08d82
InD1909 <ilyado1909@gmail.com>
2024-06-24 14:41:23 +0300

    .

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+
+// src/App.jsx
+
+import Product from './Product';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
